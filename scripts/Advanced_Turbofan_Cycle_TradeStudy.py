9396e88ad8f57b99053bfb90c831e8576085acf3
Darin Catton <dcatton@arizona.edu>
2026-03-06 00:08:27 -0700

    (I assumed the orignal script was for parametric cycle analysis of real turbofans, so I added the according equations). Added polytropic efficiencies, bypass velocity ratios, added different specific heats and constant pressure specific heats for compressor and turbine. Moved mach number into turbofan cycle function. Mach number varies with velocity and temperature(easier to have computer calculate). Corrected mdot_core to mdot_total. Added low heating value of fuel. Added if statement to check eta_r and adjust inlet total pressure ratio. Also added the pressure and temperature ratios for core and bypass. Added core and bypass mach numbers. Added thermal, propulsive, and total efficiencies. Fixed Turbine power balance equation and compressor equations needed for turbine power balance. Fixed the T/W equation (would've needed to multiply denominator by time, weight is given in the RFP, also I believe weight will be eventually calculated). Btw Sammy the changes broke the main function and the turbofan cycle function needs the return values updated. Also, I changed the OPR notation to pi_compressor, to me pi_compressor gives more clarity. Also, I added a nested function with if statements to determine if the nozzles are choked or not. Could you make sure those work. I put placeholder values for the various efficiences defined in the Params class. Eventually those will have actual values.

--- scripts/Advanced_Turbofan_Cycle_TradeStudy.py
+++ scripts/Advanced_Turbofan_Cycle_TradeStudy.py
@@ -3,31 +3,52 @@ import matplotlib.pyplot as plt
 import csv
 from dataclasses import dataclass
 
+#SI Units Only
+
 # Bartlett - 26504
 
 # ADVANCED TURBOFAN PARAMETRIC CYCLE MODEL
 
 @dataclass
 class Params:
-    gamma: float = 1.4
-    R: float = 287.0
-    cp: float = 1004.0
+    Weight:float=8
+    #Specific heat ratios and constant pressure specific heat for compressor and turbine
+    gamma_c: float = 1.4
+    gamma_t: float = 1.33
+    cp_c: float = 1006.0
+    cp_t: float = 1156.0
 
     # Ambient conditions
     T0: float = 288.15
     P0: float = 101325.0
-    M0: float = 0.0
+    V0: float = 27.78
 
     # Efficiencies
-    eta_inlet: float = 0.99
-    eta_comp: float = 0.88
-    eta_turb: float = 0.90
-    eta_noz: float = 0.95
+    'Placeholder values'
+    eta_burner:float=1
+    eta_mechanical:float=1
+
+    #Polytropic Efficiencies
+    'PlaceHolder values'
+    polytropic_eff_comp:float=1
+    polytropic_eff_fan:float=1
+    polytropic_eff_turbine:float=1
+
+    #Total Pressure Ratios
+    'Placeholder values'
+    pi_d_max:float=1
+    pi_burner:float=1
+    pi_nozzle:float=1
+    pi_fan_nozzle:float=1
+    pi_fan:float=1
+
 
     # Engine assumptions
-    mdot_core: float = 1.2
-    fuel_air_ratio: float = 0.02
-    Tt4: float = 1400.0
+    mdot_total: float = 1.2
+    'Low Heating Value of fuel Jet-A J/kg [(JP-8:43.17, Jet A1:43.33) <-- MJ/kg]'
+    h_PR:float=42.8*(10**6)
+    'Burner exit temperature'
+    Tt4: float = 1070.0
     g: float = 9.81
 
 def total_conditions(T, P, M, gamma):
@@ -35,32 +56,111 @@ def total_conditions(T, P, M, gamma):
     Pt = P * (Tt / T) ** (gamma / (gamma - 1))
     return Tt, Pt
 
-def turbofan_cycle(BPR, OPR, p: Params):
+def turbofan_cycle(BPR, pi_compressor, p: Params):
+    # Specific Gas Constants
+    global eta_r
+    global P0_P19
+    global P0_P9
+    R_c = ((p.gamma_c - 1) / p.gamma_c) * p.cp_c
+    R_t = ((p.gamma_t - 1) / p.gamma_t) * p.cp_t
+    # Speed of Sound
+    a0 = np.sqrt(p.gamma_c * R_c * p.T0)
+    M0=p.V0/a0
 
-    # Inlet
-    Tt0, Pt0 = total_conditions(p.T0, p.P0, p.M0, p.gamma)
-    Pt2 = Pt0 * p.eta_inlet
-    Tt2 = Tt0
+    'Checks Mach Number and adjusts to proper freestream efficiency'
+    if M0<1:
+        eta_r=1
+    elif M0>1:
+        eta_r=1-0.075*(M0-1)**1.35
+
+    pi_d=p.pi_d_max*eta_r
+
+    'Freestream total temp and pressure ratios'
+    tau_r=(1+((p.gamma_c-1)/2)*M0**2)
+    pi_r=tau_r**(p.gamma_c/(p.gamma_c-1))
+
+    'Burner enthalpy ratio'
+    tau_lambda=(p.cp_t*p.Tt4)/(p.cp_c*p.T0)
+
+    tau_compressor=pi_compressor**((p.gamma_c-1)/(p.gamma_c*p.polytropic_eff_comp))
+    eta_compressor=(pi_compressor**((p.gamma_c-1)/p.gamma_c)-1)/(tau_compressor-1)
+
+    tau_fan=p.pi_fan**((p.gamma_c-1)/(p.gamma_c*p.polytropic_eff_fan))
+    eta_fan=(p.pi_fan**((p.gamma_c-1)/p.gamma_c)-1)/(tau_fan-1)
+
+    fuel_air_ratio=(tau_lambda-tau_r*tau_compressor)/(((p.eta_burner*p.h_PR)/(p.cp_c*p.T0))-tau_lambda)
+
+    tau_turbine=1-(1/(p.eta_mechanical*(1+fuel_air_ratio)))*(tau_r/tau_lambda)*(tau_compressor-1+BPR*(tau_fan-1))
+    pi_turbine=tau_turbine**(p.gamma_t/((p.gamma_t-1)*p.polytropic_eff_turbine))
+    eta_turbine=(1-tau_turbine)/(1-tau_turbine**(1/p.polytropic_eff_turbine))
+
+    def exit_pressure_conditions(pi_r,pi_d,pi_fan,pi_fan_nozzle,pi_compressor,pi_turbine):
+
+        P0_P19_cond=(((p.gamma_c+1)/2)**(p.gamma_c/(p.gamma_c-1)))/(pi_r*pi_d*pi_fan*pi_fan_nozzle)
+        P0_P9_cond=(((p.gamma_t+1)/2)**(p.gamma_t/(p.gamma_t-1)))/(pi_r*pi_d*pi_compressor*pi_r*p.pi_burner*pi_turbine*p.pi_nozzle)
+
+        return P0_P19_cond, P0_P9_cond
+
+    P0_P19, P0_P9=exit_pressure_conditions(pi_r,pi_d,p.pi_fan,p.pi_fan_nozzle,pi_compressor,pi_turbine)
+
+    'Checks to see if nozzles are choked'
+    P19 = (P0_P19 ** -1) * p.P0
+    P9 = (P0_P9 ** -1) * p.P0
+
+    if P19<p.P0 and P9>p.P0:
+        'P19=P0'
+        P0_P19=1
+    elif P9<p.P0 and P19>p.P0:
+        'P9=P0'
+        P0_P9=1
+    elif P9<p.P0 and P19<p.P0:
+        'P19=P0 and P9=P0'
+        P0_P9=1
+        P0_P19=1
+
+    'Core Characteristics'
+    Pt9_P9=P0_P9*pi_r*pi_d*pi_compressor*p.pi_burner*pi_turbine*p.pi_nozzle
+    M9=np.sqrt((2/(p.gamma_t-1))*(Pt9_P9**((p.gamma_t-1)/p.gamma_t)-1))
+    'Static Temp ratio for core exit to ambient'
+    T9_T0=(tau_lambda*tau_turbine*p.cp_c)/((Pt9_P9**((p.gamma_t-1)/p.gamma_t))*p.cp_t)
+
+    V9_a0=M9*np.sqrt(((p.gamma_t*R_t)/(p.gamma_c*R_c))*T9_T0)
+    'Bypass Characteristics'
+    Pt19_P19=P0_P19*pi_r*pi_d*p.pi_fan*p.pi_fan_nozzle
+    M19 = np.sqrt((2 / (p.gamma_c - 1)) * (Pt19_P19 ** ((p.gamma_c - 1) / p.gamma_c) - 1))
+    T19_T0=(tau_r*tau_fan)/(Pt19_P19**((p.gamma_c-1)/p.gamma_c))
+    V19_a0=M19*np.sqrt(T19_T0)
+
+    'Specific Thrust, F/(total mass flow)'
+    Specific_Thrust=(a0/(1+BPR))*(((1+fuel_air_ratio)*V9_a0-M0+(1+fuel_air_ratio)*((R_t*T9_T0)/(R_c*V9_a0))*((1-P0_P9)/p.gamma_c))+((BPR*a0)/(1+BPR))*(V19_a0-M0+(T19_T0/V19_a0)*((1-P0_P19)/p.gamma_c)))
+
+    SFC=fuel_air_ratio/((1+BPR)*Specific_Thrust)
+
+    'Propulsive Efficiency'
+    eta_propulsive=(2*M0*((1+fuel_air_ratio)*V9_a0+BPR*V19_a0-(1+BPR)*M0))/((1+fuel_air_ratio)*(V9_a0**2)+BPR*(V19_a0**2)-(1+BPR)*(M0**2))
+
+    'Thermal Efficiency'
+    eta_thermal=((a0**2)*((1+fuel_air_ratio)*(V9_a0**2)+BPR*(V19_a0**2)-(1+BPR)*(M0**2)))/(2*fuel_air_ratio*p.h_PR)
+
+    'Total Efficiency'
+    eta_total=eta_propulsive*eta_thermal
+
 
     # Compressor
-    Pt3 = Pt2 * OPR
-    tau_c = OPR ** ((p.gamma - 1) / p.gamma)
-    Tt3 = Tt2 * (1 + (tau_c - 1) / p.eta_comp)
+    Tt0, Pt0=total_conditions(p.T0,p.P0,M0,p.gamma_c)
+    Tt2=Tt0
+    Tt3=tau_compressor*Tt2
+    mdot_core=p.mdot_total/(1+BPR)
 
     # Turbine power balance
-    Wc = p.mdot_core * p.cp * (Tt3 - Tt2)
-    delta_T_turb = Wc / (p.mdot_core * p.cp)
-    Tt5 = p.Tt4 - delta_T_turb / p.eta_turb
+    Wc = mdot_core * p.cp_c * (Tt3 - Tt2)
 
-    # Nozzle exit velocity
-    Te = 0.9 * Tt5
-    Ve = np.sqrt(2 * p.eta_noz * p.cp * (Tt5 - Te))
+    'Thrust to weight ratio'
+    tw = Specific_Thrust / p.Weight
 
-    mdot_total = p.mdot_core * (1 + BPR)
-    thrust = mdot_total * Ve
-    tsfc = (p.fuel_air_ratio * p.mdot_core) / thrust
-    tw = thrust / (mdot_total * p.g)
-
+    V9=V9_a0*a0
+    V19=V19_a0*a0
+    'needs to return Specific_Thrust, (Thermal, propulsive, & total efficiency), Mach 9 and 19, V19_a0, V9_a0, Pt19/P19, Pt9/P9, SFC'
     return thrust, tsfc, tw
 
 def main():
